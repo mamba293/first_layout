c3043de82be00484207a14a991d7e88fe1e84ede
Mamba <haleckijigor13@gmail.com>
2025-07-17 16:14:26 +0300

    finish video component

--- youtube/src/components/Player/VideoPlayer.jsx
+++ youtube/src/components/Player/VideoPlayer.jsx
@@ -1,8 +1,69 @@
+import "./VideoPlayer.css";
+
 export default function VideoPlayer() {
  return (
   <div className="video__player-container">
-   
-   <video src="/src/assets/videos/video.mp4" width="1363" height="700" controls></video>
+   <div className="player-container">
+    <video
+     src="/src/assets/videos/video.mp4"
+     className="video"
+     width="1363"
+     height="700"
+    ></video>
+
+    <div className="progress-wrapper">
+     <div className="progress-times">
+      <p>1:34</p>
+      <p>19:00</p>
+     </div>
+     <div className="progress-overlay">
+      <div className="progress-bar"></div>
+     </div>
+    </div>
+
+    <div className="controls">
+     <div className="controls_pannel-left">
+      <div className="controls-play">
+       <img
+        src="/src/assets/videos/controls/Pause.svg"
+        alt="pause"
+        className="pause_btn"
+       />
+      </div>
+      <div className="controls-next">
+       <img
+        src="/src/assets/videos/controls/Next.svg"
+        alt="next"
+        className="play_next"
+       />
+      </div>
+      <div className="volume_container">
+       <img src="/src/assets/videos/controls/Volume.svg" alt="" />
+       <input
+        type="range"
+        className="volume_bar"
+        min="0"
+        max="100"
+        defaultValue="50"
+       />
+      </div>
+     </div>
+     <div className="controls_pannel-right">
+      <div>
+       <img src="/src/assets/videos/controls/Subtitles.svg" alt="" />
+      </div>
+      <div>
+       <img src="/src/assets/videos/controls/Settings.svg" alt="" />
+      </div>
+      <div>
+       <img src="/src/assets/videos/controls/Size.svg" alt="" />
+      </div>
+      <div>
+       <img src="/src/assets/videos/controls/FullScreen.svg" alt="" />
+      </div>
+     </div>
+    </div>
+   </div>
 
    <div className="video__player-description">
     <h1 className="description-name">Dude You Re Getting A Telescope</h1>
@@ -28,7 +89,6 @@ export default function VideoPlayer() {
      </div>
     </div>
    </div>
-   
   </div>
  );
 }
--- youtube/src/pages/VideoPlayerPage.jsx
+++ youtube/src/pages/VideoPlayerPage.jsx
@@ -1,11 +1,12 @@
 import Header from "../components/header/Heder";
 import VideoPlayer from "../components/Player/VideoPlayer";
 
+
 export default function VideoPlayerPage() {
  return (
-  <>
+  <body>
    <Header />
-    <VideoPlayer/>
-  </>
+   <VideoPlayer />
+  </body>
  );
 }
