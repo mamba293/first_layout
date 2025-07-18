36067376ca3a93d309657f7a94c81caf6008f598
Mamba <haleckijigor13@gmail.com>
2025-07-18 06:55:38 +0300

    change config

--- youtube/vite.config.js
+++ youtube/vite.config.js
@@ -3,9 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg'],
+  assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg'],
   build: {
     outDir: 'dist', // Vercel будет использовать её
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (/\.(png|jpe?g|svg)$/.test(assetInfo.name)) {
+            return 'assets/images/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
   },
 })
-
