e9b4bce17a222eabc9865cbf79efc43da1481914
Mamba <haleckijigor13@gmail.com>
2025-07-18 06:14:44 +0300

    add router

--- youtube/src/main.jsx
+++ youtube/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import VideoPlayerPage from "./pages/VideoPlayerPage";
-
+import { BrowserRouter } from "react-router-dom";
 
 const root = document.getElementById("root");
 
 createRoot(root).render(
  <StrictMode>
-  <VideoPlayerPage />
+  <BrowserRouter>
+   <VideoPlayerPage />
+  </BrowserRouter>
  </StrictMode>
 );
